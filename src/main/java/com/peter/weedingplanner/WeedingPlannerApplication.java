f830c12f372c3b1e42d11966c7e3950e2e4d3f8c
PeterAmisy <amisy.peter@gmail.com>
2022-12-12 17:19:23 +0100

    first commit closes #
    Signed-off-by: PeterAmisy <amisy.peter@gmail.com>

---
+++ src/main/java/com/peter/weedingplanner/WeedingPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.peter.weedingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeedingPlannerApplication.class, args);
+	}
+
+}
